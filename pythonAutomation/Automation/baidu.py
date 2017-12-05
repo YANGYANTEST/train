d75d0fb00914398a8cb4a6a737b23300af15931c
YANGYANTEST <15221629532@1163.com>
2017-12-05 08:13:05 +0800

    baiduAutomation

--- pythonAutomation/Automation/baidu.py
+++ pythonAutomation/Automation/baidu.py
@@ -4,7 +4,7 @@
 
 import time
 from selenium import webdriver
-driver=webdriver.Chrome()
+driver=webdriver.Ie()
 driver.get("http://www.baidu.com")
 driver.find_element_by_id("kw").send_keys("selenium")
 driver.find_element_by_id("su").click()

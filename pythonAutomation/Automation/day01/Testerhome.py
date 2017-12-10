41b6159289efe5a643b8eecc9a482c2c3698686f
YANGYANTEST <15221629532@1163.com>
2017-12-10 20:17:18 +0800

    day01_Automation

--- pythonAutomation/Automation/day01/Testerhome.py
+++ pythonAutomation/Automation/day01/Testerhome.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 import time
-#注册
+
 driver=webdriver.Chrome()
 driver.get("https://testerhome.com/")
 driver.find_element_by_xpath("//html/body/div[1]/nav/div/ul[1]/li[1]/a").click()
@@ -11,17 +11,24 @@ driver.find_element_by_id("user_email").send_keys("15221629532@163.com")
 driver.find_element_by_id("user_password").send_keys("123456")
 driver.find_element_by_name("user[password_confirmation]").send_keys("123456")
 time.sleep(8)
-driver.find_element_by_name("commit").click()
-driver.quit()
+# driver.find_element_by_name("commit").click() #点击提交注册信息
+driver.find_element_by_xpath('//*[@id="new_user"]/div[8]/a').click()    #点击登陆
+# driver.quit()
+time.sleep(5)
 
 #login
 
-# driver = webdriver.Chrome()
-# driver.get("https://testerhome.com/")
-# driver.find_element_by_xpath('/html/body/div[1]/nav/div/ul[1]/li[2]/a').click()
-# time.sleep(5)
-# driver.find_element_by_name("user[login]").send_keys('yangyan')
-# driver.find_element_by_name("user[password]").send_keys('123456')
+driver=webdriver.Chrome()
+driver.get("https://testerhome.com/")
+driver.find_element_by_xpath('/html/body/div[1]/nav/div/ul[1]/li[2]/a').click()
+time.sleep(5)
+driver.find_element_by_name("user[login]").send_keys('yangyan')
+driver.find_element_by_name("user[password]").send_keys('123456')
 # driver.find_element_by_name("user[remember_me]").click()
-# driver.find_element_by_name("commit").submit()
+driver.find_element_by_name("commit").submit()
+time.sleep(3)
 # driver.quit()
+driver.find_element_by_xpath('//*[@id="main-nav-menu"]/ul/li[1]/a').click()
+driver.find_element_by_xpath('//*[@id="main"]/div/div[2]/div[1]/div[3]').click()
+
+
